e17352c1bfeefeccea914bfb127dae77f315a0b3
Thorsten Herfurtner <88311102+totti-rdz@users.noreply.github.com>
2023-05-13 23:14:10 +0000

    05

--- challenges/05uncensor.js
+++ challenges/05uncensor.js
@@ -1,11 +1,21 @@
 function uncensor(infected, remedy) {
   // Write your code here
-
+  let result = "";
+  let remedyIndex = 0;
+  for (let i = 0; i < infected.length; i++) {
+    if (infected[i] === "*") {
+      result += remedy[remedyIndex];
+      remedyIndex++;
+    } else {
+      result += infected[i]
+    }
+  }
+  return result
 }
 
 
-const { assert } = require('chai'); 
-describe("Tests", function() {
+const { assert } = require('chai');
+describe("Tests", function () {
   it("tests", () => {
     assert.deepEqual(uncensor('H*l** W*l*', 'aloet'), 'Hallo Welt');
     assert.deepEqual(uncensor('G*h*im*r T*xt', 'eeee'), 'Geheimer Text');
