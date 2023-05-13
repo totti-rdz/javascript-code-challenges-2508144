ff26841a57156e24b60e14da2b8aa4b6a8987c57
Thorsten Herfurtner <88311102+totti-rdz@users.noreply.github.com>
2023-05-13 23:02:34 +0000

    03

--- challenges/03rockpaperscissors.js
+++ challenges/03rockpaperscissors.js
@@ -1,9 +1,25 @@
 const rps = (p1, p2) => {
   // Write your code here
+  if (p1 === p2) return 0;
+
+  const map = {
+    paper: 1,
+    scissors: 2,
+    rock: 3,
+  }
+
+  // in a sequence of paper(1) < scissors(2) < rock(3) 
+  // the greater wins as long as the distance is 1, else the smaller wins
+  // can be probably solved with modulo in a more elegant way
+  if (Math.abs(map[p1] - map[p2]) === 1) {
+    return map[p1] > map[p2] ? 1 : 2;
+  } else {
+    return map[p2] < map[p1] ? 2 : 1;
+  }
 
 }
 
-const { assert } = require('chai'); 
+const { assert } = require('chai');
 describe('Tests', () => {
   it("tests", () => {
     assert.equal(rps('rock', 'scissors'), 1);
