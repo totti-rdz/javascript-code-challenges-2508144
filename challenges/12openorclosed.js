33509160edb44a77b8e8685c0bbf51dc516abc89
Thorsten Herfurtner <88311102+totti-rdz@users.noreply.github.com>
2023-05-14 11:56:56 +0000

    12

--- challenges/12openorclosed.js
+++ challenges/12openorclosed.js
@@ -1,6 +1,27 @@
 function isOpen(timestamp) {
   // Write your code here
+  const openingHours = {
+    0: [],
+    1: [8, 12, 14, 18],
+    2: [8, 12, 14, 18],
+    3: [8, 12],
+    4: [8, 12, 14, 20],
+    5: [8, 12, 14, 18],
+    6: [8, 13],
+  };
 
+  const date = new Date(timestamp);
+
+  for (let i = 0; i < openingHours[date.getDay()].length; i += 2) {
+    if (
+      date.getHours() >= openingHours[date.getDay()][i] &&
+      date.getHours() < openingHours[date.getDay()][i + 1]
+    ) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 
@@ -13,6 +34,5 @@ describe("Tests", () => {
     assert.equal(isOpen('2026-7-15 16:00'), false); // Wednesday
     assert.equal(isOpen('2026-7-18 14:00'), false); // Saturday
     assert.equal(isOpen('2026-7-19 09:00'), false); // Sunday
-
   });
 });
