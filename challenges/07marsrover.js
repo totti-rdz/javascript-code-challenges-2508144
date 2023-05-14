d1b5c752edcf9df0e1d99ff6d4876c447ea2ccd2
Thorsten Herfurtner <88311102+totti-rdz@users.noreply.github.com>
2023-05-14 00:11:00 +0000

    07

--- challenges/07marsrover.js
+++ challenges/07marsrover.js
@@ -1,6 +1,32 @@
 function rover(way) {
   // Write your code here
+  const directions = ["north", "west", "south", "east"]
+  let currentDirIndex = 0;
+  const map = {
+    north: { x: 0, y: 1 },
+    west: { x: 1, y: 0 },
+    south: { x: 0, y: -1 },
+    east: { x: -1, y: 0 },
+  }
 
+  const walk = (currentDirIndex) => map[directions[currentDirIndex]]
+
+  const position = { x: 0, y: 0 }
+  way.split("").forEach((elem) => {
+    switch (elem) {
+      case "r":
+        currentDirIndex = ((currentDirIndex + 1) % 4);
+        break;
+      case "l":
+        currentDirIndex = ((currentDirIndex - 1) % 4);
+        break;
+      case "f":
+        position.x += walk(currentDirIndex).x;
+        position.y += walk(currentDirIndex).y;
+        break;
+    }
+  });
+  return position;
 }
 
 const { assert } = require('chai');
