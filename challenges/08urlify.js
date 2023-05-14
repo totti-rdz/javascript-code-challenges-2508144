c4c252744cea3ef3c029b35a43bcc859b1dfefac
Thorsten Herfurtner <88311102+totti-rdz@users.noreply.github.com>
2023-05-14 00:19:26 +0000

    08

--- challenges/08urlify.js
+++ challenges/08urlify.js
@@ -1,7 +1,12 @@
 function urlify(s) {
   // Write your code here
+  const map = {
+    " ": "%20",
+    "ö": "%C3%B6"
+  }
 
-  
+  return s.split("").map((elem) => (Object.keys(map).includes(elem)) ? map[elem] : elem).join("")
+
 }
 
 const { assert } = require('chai');
@@ -9,5 +14,6 @@ describe("Tests", () => {
   it("tests", () => {
     assert.equal(urlify('Hallo Welt'), 'Hallo%20Welt');
     assert.equal(urlify('Vielen Dank, Thomas'), 'Vielen%20Dank,%20Thomas');
-    assert.equal(urlify('Schöne Bilder!'), 'Sch%C3%B6ne%20Bilder!');  });
+    assert.equal(urlify('Schöne Bilder!'), 'Sch%C3%B6ne%20Bilder!');
+  });
 });
